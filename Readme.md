2584d99d5c08b56add19a9065bf4d06b2688b4ca
kaniket7209 <kaniket7209@gmail.com>
2022-01-16 14:46:12 +0530

    update

---
+++ Readme.md
@@ -0,0 +1,10 @@
+To Run this script just install cheerio and request using mpm following below command
+
+npm install cheerio
+npm install request
+
+To uss=e this just write the command as follows
+
+node app.js "Nokia 6.1" 
+
+node app.js "Shoes"
---
+++ app.js
@@ -0,0 +1,40 @@
+
+const inputArr = process.argv.slice(2)
+// console.log(inputArr[0])
+let inpstring = inputArr[0].split(" ")
+const name = inpstring.join("+")
+// console.log(name)
+const request = require('request')
+const cheerio = require('cheerio')
+
+let url = `https://www.amazon.in/s?k=${name}`;
+console.log(url);
+request(url, function (err, response, html) {
+    if (err) {
+        console.log("Error");
+    }
+    else {
+        // console.log(html);
+        fetchdata(html);
+    }
+})
+function fetchdata(html) {
+    let $ = cheerio.load(html);
+
+    let nameArr = $(".a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2")
+    let priceArr = $("span.a-price-whole")
+    let badgeArr =  $("span.a-badge-text")
+    let starsArr = $("span.a-icon-alt")
+    let starsContribution = $("span.a-size-base")
+
+    for (let i = 0; i < nameArr.length ; i++) {
+        let nameELe = $(nameArr[i]).text()
+        let priceEle = $(priceArr[i]).text()
+        let badgeEle = $(badgeArr[i]).text()
+        let starsEle = $(starsArr[i]).text()
+        let starsContriEle = $(starsContribution[i]).text()
+        console.log(nameELe, "--", priceEle,"--",badgeEle,"--",starsEle,"--",starsContriEle)
+
+    }
+
+}
