c319a11167d476d7b1be549d7ab8db17f31693bd
kaniket7209 <kaniket7209@gmail.com>
2022-01-16 15:49:47 +0530

    update

--- Readme.md
+++ Readme.md
@@ -8,3 +8,9 @@ To uss=e this just write the command as follows
 node app.js "Nokia 6.1" 
 
 node app.js "Shoes"
+
+If you want to convert data into pdf 'install pdfkit using command npm install pdfkit
+
+ANd if you want to convert data into xlxs then install xlsx using command
+npm install xlsx
+
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const pdf = require('pdfkit');
+const path = require('path');
 const fs = require('fs');
+const xlsx = require('xlsx')
 const inputArr = process.argv.slice(2)
+
 // console.log(inputArr[0])
 let inpstring = inputArr[0].split(" ")
 const name = inpstring.join("+")
@@ -38,11 +41,59 @@ function fetchdata(html) {
         // console.log(nameELe, "--", "₹"+priceEle,"--",badgeEle,"--",starsEle,"--",starsContriEle)
         arr.push({nameELe,priceEle,badgeEle,starsEle,starsContriEle})
         console.log((arr));
+        processData(nameELe,priceEle,badgeEle,starsEle,starsContriEle)
         
     }
+
+   /* To convert into pdf uncomment this
     let pdfDoc = new pdf
     pdfDoc.pipe(fs.createWriteStream('Data.pdf'));
     pdfDoc.text(JSON.stringify(arr));
-    pdfDoc.end();
+    pdfDoc.end();*/
 
+}
+
+function processData(nameELe,priceEle,badgeEle,starsEle,starsContriEle){
+    let dirpath = path.join(__dirname,"Amazon")
+    dircreater(dirpath)
+    data = "Data"
+    let filePath= path.join(dirpath ,data+".xlsx");
+    let content = excelReader(filePath,data);
+    let dataObj = {
+        "Name " : nameELe,
+        "Price ": priceEle,
+        "Badge" : badgeEle,
+        "No. of stars": starsEle,
+        "Stars Contributors" : starsContriEle
+    }
+    content.push(dataObj)
+    excelWriter(filePath,content,data);
+
+}
+
+
+function dircreater(filepath) {
+    if (fs.existsSync(filepath) == false) {
+        fs.mkdirSync(filepath);
+    }
+}
+function excelWriter(filePath, json, sheetName) {
+    //creates new workbook
+    let newwb = xlsx.utils.book_new();
+    let newws = xlsx.utils.json_to_sheet(json);
+    xlsx.utils.book_append_sheet(newwb, newws, sheetName);
+    xlsx.writeFile(newwb, filePath);
+}
+function excelReader(filePath, sheetName) {
+    // //creates new sheet - (data -> excel format )
+    // // add sheet(newwb,ws,sheet name)
+    // // file path
+    // read
+    if (fs.existsSync(filePath) == false) {
+        return [];
+    }
+    let wb = xlsx.readFile(filePath);
+    let exceldata = wb.Sheets[sheetName];//sheet name inside wb
+    let ans = xlsx.utils.sheet_to_json(exceldata);
+    return ans;
 }
