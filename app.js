bc915963b0d0a788c7bf044e5299a5867eb1b220
kaniket7209 <kaniket7209@gmail.com>
2022-01-16 15:12:53 +0530

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+const pdf = require('pdfkit');
+const fs = require('fs');
 const inputArr = process.argv.slice(2)
 // console.log(inputArr[0])
 let inpstring = inputArr[0].split(" ")
@@ -26,15 +27,22 @@ function fetchdata(html) {
     let badgeArr =  $("span.a-badge-text")
     let starsArr = $("span.a-icon-alt")
     let starsContribution = $("span.a-size-base")
-
+    
+    let arr = []
     for (let i = 0; i < nameArr.length ; i++) {
         let nameELe = $(nameArr[i]).text()
         let priceEle = $(priceArr[i]).text()
         let badgeEle = $(badgeArr[i]).text()
         let starsEle = $(starsArr[i]).text()
         let starsContriEle = $(starsContribution[i]).text()
-        console.log(nameELe, "--", priceEle,"--",badgeEle,"--",starsEle,"--",starsContriEle)
-
+        // console.log(nameELe, "--", "₹"+priceEle,"--",badgeEle,"--",starsEle,"--",starsContriEle)
+        arr.push({nameELe,priceEle,badgeEle,starsEle,starsContriEle})
+        console.log((arr));
+        
     }
+    let pdfDoc = new pdf
+    pdfDoc.pipe(fs.createWriteStream('Data.pdf'));
+    pdfDoc.text(JSON.stringify(arr));
+    pdfDoc.end();
 
 }
